ff59976aaabe0944e08475aba5ec732f9997ad3b
somdats <somdats@gmail.com>
2022-05-05 17:30:08 +0200

    opencv build added

---
+++ 3rdParty/opencv_build/include/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/3rdParty/opencv_source/include
+# Build directory: D:/3rdParty/opencv_build/include
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
