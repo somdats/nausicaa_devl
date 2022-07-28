38df58170d684c451bddb017a5dab91be14acd4d
somdats <somdats@gmail.com>
2022-07-28 09:28:56 +0200

    commit reversed

--- NAUSICAA_VR_API/NAUSICAA_API_SERVER/src/nausicaa_api_server.cpp
+++ NAUSICAA_VR_API/NAUSICAA_API_SERVER/src/nausicaa_api_server.cpp
@@ -21,10 +21,6 @@ bool camerasOn[6];
 std::mutex m;
 std::condition_variable condv;
 bool picked = false;
-bool pick_point;
-int pick_x, pick_y;
-float picked_point[3];
-
 
 
 Server serverComm, serverStream;
